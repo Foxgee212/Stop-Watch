1ee48fd61387a77688d84fa4719f4b3bd9958a89
Mrshrek <132598235+Foxgee212@users.noreply.github.com>
2025-08-09 23:46:30 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import MyComponentA from "./MyComponentsA";
+
+
+function App() {
+  return(
+    <> 
+      <MyComponentA /><br />
+      <MyComponentA /><br />
+      <MyComponentA /><br />
+
+    </>
+  );
+}
+
+export default App;
+
+
+ 
